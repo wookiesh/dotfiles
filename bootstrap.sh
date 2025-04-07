226dd0484510da25815df7ef28ba9daa3583fc2d
Joseph Piron <git@miom.be>
2025-04-07 20:36:50 +0200

    remove make dependency

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,43 +8,43 @@ ACTION=${1:-dev}
 TARGET="$HOME"
 
 function stow_packages() {
-  local packages=("$@")
-  for pkg in "${packages[@]}"; do
-    echo "Stowing '$pkg'..."
-    stow --verbose --stow --target "$TARGET" "$pkg"
-  done
+    local packages=("$@")
+    for pkg in "${packages[@]}"; do
+        echo "Stowing '$pkg'..."
+        stow --verbose --stow --target "$TARGET" "$pkg"
+    done
 }
 
 function delete_packages() {
-  local packages=("$@")
-  for pkg in "${packages[@]}"; do
-    echo "Unstowing '$pkg'..."
-    stow --verbose --target "$TARGET" --delete "$pkg"
-  done
+    local packages=("$@")
+    for pkg in "${packages[@]}"; do
+        echo "Unstowing '$pkg'..."
+        stow --verbose --target "$TARGET" --delete "$pkg"
+    done
 }
 
 case "$ACTION" in
-  dev)
+dev)
     # Automatically detect all folders (except some common ones to exclude)
     pkgs=()
     for dir in */; do
-      [[ "$dir" =~ ^(\.git|scripts|README|bootstrap.sh) ]] && continue
-      pkgs+=("${dir%/}")
+        [[ "$dir" =~ ^(\.git|scripts|README|bootstrap.sh) ]] && continue
+        pkgs+=("${dir%/}")
     done
     stow_packages "${pkgs[@]}"
     ;;
-  remote)
+remote)
     stow_packages zsh vim tmux starship git
     ;;
-  delete)
+delete)
     # Delete all folders in current dir
     pkgs=()
     for dir in */; do
-      pkgs+=("${dir%/}")
+        pkgs+=("${dir%/}")
     done
     delete_packages "${pkgs[@]}"
     ;;
-  *)
+*)
     echo "Usage: ./bootstrap.sh [dev|remote|delete]"
     exit 1
     ;;
